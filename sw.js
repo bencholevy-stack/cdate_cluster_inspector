a2356fb9a15a9db7f32cc88c2311ebf456ca86c1
bencholevy-stack <bencho.levy@gmail.com>
2026-09-06 20:11:19 +0300

    PWA 2.7.9 refresh cache for visible overlays and plantation map guard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='dci-pwa-2.7.8';
+const CACHE_NAME='dci-pwa-2.7.9';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
